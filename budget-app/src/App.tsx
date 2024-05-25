88c8e7997ac6e53f7bce12d974f9112248df29e4
Angela Wang <angelawang.aw@outlook.com>
2024-05-25 13:18:37 -0400

    Implement signup page

--- budget-app/src/App.tsx
+++ budget-app/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./LoginPage/LoginPage";
 import HomePage from "./HomePage/HomePage";
+import SignupPage from "./SignupPage/SignupPage";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/home" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
       </Routes>
     </BrowserRouter>
   );
