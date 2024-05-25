68623ace3efa95af51ddbce400e2595488766603
Angela Wang <angelawang.aw@outlook.com>
2024-05-25 12:56:40 -0400

    Separate Login and LoginForm

---
+++ budget-app/src/login/LoginForm.tsx
@@ -0,0 +1,26 @@
+import { Box, Button, FormControl, FormLabel, Input } from "@chakra-ui/react";
+import React from "react";
+
+const LoginForm: React.FC = () => {
+  return (
+    <Box p={6}>
+      <FormControl>
+        <FormLabel>Email address</FormLabel>
+        <Input type="email" />
+        <FormLabel>Password</FormLabel>
+        <Input type="password" />
+      </FormControl>
+      <Button
+        mt={5}
+        colorScheme="green"
+        onClick={() => {
+          console.log("Add functionality");
+        }}
+      >
+        Login
+      </Button>
+    </Box>
+  );
+};
+
+export default LoginForm;
--- budget-app/src/login/LoginPage.tsx
+++ budget-app/src/login/LoginPage.tsx
@@ -1,33 +1,8 @@
-import {
-  Box,
-  Button,
-  FormControl,
-  FormHelperText,
-  FormLabel,
-  Input,
-} from "@chakra-ui/react";
 import React from "react";
+import LoginForm from "./LoginForm";
 
 const Login: React.FC = () => {
-  return (
-    <Box p={6}>
-      <FormControl>
-        <FormLabel>Email address</FormLabel>
-        <Input type="email" />
-        <FormLabel>Password</FormLabel>
-        <Input type="password" />
-      </FormControl>
-      <Button
-        mt={5}
-        colorScheme="green"
-        onClick={() => {
-          console.log("Add functionality");
-        }}
-      >
-        Login
-      </Button>
-    </Box>
-  );
+  return <LoginForm />;
 };
 
 export default Login;
